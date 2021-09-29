8fb9ea0686da7069a068734279c5c223cdee1587
simlogo <simlogo17@gmail.com>
2021-09-29 12:22:52 +0100

    Code refactored

---
+++ src/components/Scroll.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Scroll = (props) => {
+
+    return (
+        <div style={{overflow:'scroll', border: '3px solid #0ccac4', height:'900px'}}> 
+            {props.children}
+        </div>
+    )
+};
+
+export default Scroll;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import App from './containers/App';
 import 'tachyons';
+
 
 
 
